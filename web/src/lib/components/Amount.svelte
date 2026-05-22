b16639d50d29f15a986b48ad4a4b317ed5815992
Abram <git.skimming549@passinbox.com>
2026-05-22 01:09:47 +0100

    feat (web): add Amount component with hidden amounts support

---
+++ web/src/lib/components/Amount.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { hideAmounts } from "$lib/stores";
+
+    export let value: string;
+    let className: string = "";
+    export { className as class };
+</script>
+
+<span class={className}>
+    {#if $hideAmounts}
+        ••••
+    {:else}
+        {value}
+    {/if}
+</span>
